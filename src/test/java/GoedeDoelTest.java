ed93693915904e74f10eb27d09c2b55b4c9b6cee
Lucinda Flores <lucindafloresbriones@gmail.com>
2026-07-09 13:45:30 +0200

    Test for GoedeDoel method equals

--- src/test/java/GoedeDoelTest.java
+++ src/test/java/GoedeDoelTest.java
@@ -27,4 +27,13 @@ class GoedeDoelTest {
         assertThat(doel.getOpbrengst()).isZero();
     }
 
+    @Test
+    void doelenMetDezelfdeNaamZijnGelijk() {
+        assertThat(doel).isEqualTo(new GoedeDoel(NAAM));
+    }
+    @Test
+    void doelenMetVerschillendeNaamZijnVerschillend() {
+        assertThat(doel).isNotEqualTo(new GoedeDoel("WWF"));
+    }
+
 }
