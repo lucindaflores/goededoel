b9dad5b454e5c437ed427ed538f97322f96c6c99
Lucinda Flores <lucindafloresbriones@gmail.com>
2026-07-09 13:06:05 +0200

    First commit

---
+++ src/test/java/GoedeDoelTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GoedeDoelTest {
+
+    private static final String NAAM = "CliniClowns";
+    private GoedeDoel doel;
+
+    @BeforeEach
+    void beforeEach() {
+        doel = new GoedeDoel(NAAM);
+    }
+
+    @Test
+    void getNaam() {
+        assertThat(doel.getNaam()).isEqualTo(NAAM);
+    }
+
+}
