6ad1f6895aa48a25fcf13536da43cfc38ecd98ba
Lucinda Flores <lucindafloresbriones@gmail.com>
2026-07-09 14:08:36 +0200

    Javadoc added to pom.xml

--- src/main/java/GoedeDoel.java
+++ src/main/java/GoedeDoel.java
@@ -1,18 +1,35 @@
 import java.math.BigDecimal;
 
+/**
+ * Een <strong>goed doel</strong> waarvoor men geld inzamelt
+ * @author Joe Dalton
+ */
+
 public class GoedeDoel {
 
     private final String naam;
     private final BigDecimal opbrengst = BigDecimal.ZERO;
 
+    /**
+     * Maakt een GoedeDoel object
+     * @param naam De naam van het goede doel
+     */
     public GoedeDoel(String naam) {
         this.naam = naam;
     }
 
+    /**
+     * Geeft de naam terug
+     * @return de naam
+     */
     public String getNaam() {
         return naam;
     }
 
+    /**
+     * Geeft de opbrengst terug
+     * @return de opbrengst
+     */
     public BigDecimal getOpbrengst() {
         return opbrengst;
     }
